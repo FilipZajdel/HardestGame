e9f00ac607908c93df248461d5104aed1e7ec43a
FilipZajdel <filip97.zajdel@gmail.com>
2019-07-12 21:20:35 +0200

    tests for shape

--- components_test/shape_test.c
+++ components_test/shape_test.c
@@ -14,3 +14,19 @@ TEST(deleting_shape, shape) {
   shape_remove(&shape);
   EXPECT_EQ(NULL, shape);
 }
+
+TEST(containing_other_shape, shape){
+  shape_t* shape = shape_make(RECTANGLE);
+  shape_t* other_shape = shape_make(RECTANGLE);
+
+  EXPECT_EQ(0, shape->contains_other(shape, 0, 0, 5, 5, other_shape));
+
+  shape_remove(&shape);
+  shape_remove(&other_shape);
+}
+
+TEST(containing_point_in_shape, shape){
+  shape_t* shape = shape_make(RECTANGLE);
+  EXPECT_EQ(0, shape->contains(shape, 0, 0, 5, 5));
+  shape_remove(&shape);
+}
